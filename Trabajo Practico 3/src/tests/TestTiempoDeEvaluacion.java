6064258ed74fa041e8af1324aa28f09efc6130c4
Luca Frances <francesluca@hotmail.com>
2016-09-19 01:43:49 -0300

    Consigna realizada hasta evaluar Math.Pow().

---
+++ Trabajo Practico 3/src/tests/TestTiempoDeEvaluacion.java
@@ -0,0 +1,84 @@
+package tests;
+
+import java.io.FileNotFoundException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import polinomio.Polinomio;
+
+public class TestTiempoDeEvaluacion {
+
+	private Calendar tiempoInicio;
+	private Calendar tiempoFin;
+	private Polinomio polinomio;
+
+	public TestTiempoDeEvaluacion(String Path) throws FileNotFoundException {
+		polinomio = new Polinomio(Path);
+	}
+
+	public long tiempoMultiplicacionesSucesivas(double x) {
+
+		tiempoInicio = new GregorianCalendar();
+
+		polinomio.evaluarMultiplicacionesSucesivas(x);
+
+		tiempoFin = new GregorianCalendar();
+
+		return (long)(tiempoFin.getTimeInMillis() - tiempoInicio.getTimeInMillis());
+	}
+	
+	public long tiempoRecursiva(double x) {
+
+		tiempoInicio = new GregorianCalendar();
+
+		polinomio.evaluarRecursiva(x);
+
+		tiempoFin = new GregorianCalendar();
+
+		return (long)(tiempoFin.getTimeInMillis() - tiempoInicio.getTimeInMillis());
+	}
+	
+	public long tiempoRecursivaMejorada(double x) {
+
+		tiempoInicio = new GregorianCalendar();
+
+		polinomio.evaluarRecursivaMejorada(x);
+
+		tiempoFin = new GregorianCalendar();
+
+		return (long)(tiempoFin.getTimeInMillis() - tiempoInicio.getTimeInMillis());
+	}
+	
+	public long tiempoProgramacionDinamica(double x) {
+
+		tiempoInicio = new GregorianCalendar();
+
+		polinomio.evaluarProgramacionDinamica(x);
+
+		tiempoFin = new GregorianCalendar();
+
+		return (long)(tiempoFin.getTimeInMillis() - tiempoInicio.getTimeInMillis());
+	}
+	
+	public long tiempoMathPow(double x) {
+
+		tiempoInicio = new GregorianCalendar();
+
+		polinomio.evaluarMathPow(x);
+
+		tiempoFin = new GregorianCalendar();
+
+		return (long)(tiempoFin.getTimeInMillis() - tiempoInicio.getTimeInMillis());
+	}
+	
+	public long tiempoMejorada(double x) {
+
+		tiempoInicio = new GregorianCalendar();
+
+		polinomio.evaluarMejorada(x);
+
+		tiempoFin = new GregorianCalendar();
+
+		return (long)(tiempoFin.getTimeInMillis() - tiempoInicio.getTimeInMillis());
+	}
+}
